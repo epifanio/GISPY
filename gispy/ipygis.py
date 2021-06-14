377ebce75dc0af51c482d7a09c19b382a6e5bb95
epifanio <epiesasha@me.com>
2021-06-14 13:23:48 +0200

    adding move up/down layers

--- gispy/ipygis.py
+++ gispy/ipygis.py
@@ -6,8 +6,6 @@ from traitlets import Unicode
 import xml.etree.ElementTree as ET
 
 
-
-
 class ZTWMSLayer(WMSLayer):
     time = Unicode("").tag(sync=True, o=True)
     elevation = Unicode("").tag(sync=True, o=True)
@@ -25,13 +23,17 @@ def get_center(wms):
 
 
 def get_png_legend(legend):
-    return f'<br><div id="img" style="float:left"><img style="vertical-align:middle" src="{legend}" alt=""></div>'
+    return f'<div id="img" style="float:left"><img style="horizontal-align:top" src="{legend}" width="80%" alt=""></div>'
 
 
 def get_wms_layers(wms):
     wms_layers = list(set(list(wms.contents)) - set(["WMS", "lat", "lon"]))
     wms_layers.sort()
     return wms_layers
+
+
+def get_wms_name(wms):
+    name = ET.fromstring(wms.getServiceXML().decode()).findall('.//*Layer')[0].findall('.//*Title')[0].text,
 
 
 def get_ztwms(wms_url, crs, ipygis_key):
@@ -63,23 +65,6 @@ def get_ztwms(wms_url, crs, ipygis_key):
     return ztwms, wms, wms_layers
 
 
-class move_buttons(object):
-    def __init__(self, m=None, index=None):
-        self.m=m
-        self.index=index
-        self.buttons_control = self.get_buttons()
-
-    def print_something(self, b):
-        print(b)
-
-    def get_buttons(self):
-        move_up = Button(description='^', layout=Layout(width='30px', height='30px'))
-        move_down = Button(description='v', layout=Layout(width='30px', height='30px'))
-        move_up.on_click(self.print_something(self.index))
-        move_down.on_click(self.print_something(self.index))
-        return HBox([move_up, move_down])
-
-
 class ztwms_control(object):
     def __init__(self, wms_url, crs, m=None, ipygis_key=None):
         self.wms_url = wms_url
@@ -95,26 +80,21 @@ class ztwms_control(object):
             value=True,
             description=f"<b>{self.ztwms.name}</b>",
             disabled=False,
-            indent=False
+            indent=False,
+            # layout=Layout(max_width="380px", max_height="35px")
         )
-        header = HBox([layer_header])
-        # HBox([Button(description='^', layout=Layout(width='30px', height='30px')), Button(description='^', layout=Layout(width='30px', height='30px'))])
-        ## move_up = Button(description='^', layout=Layout(width='30px', height='30px'))
-        ## move_down = Button(description='v', layout=Layout(width='30px', height='30px'))
-        ## move_up.on_click(self.map_intercat_up)
-        ## move_down.on_click(self.map_intercat_down)
-        ## head = HBox([layer_header, move_up, move_down])
-        #
+        header = HBox([layer_header],
+                      # layout=Layout(max_width="380px", max_height="35px")
+                      )
         layer_header.observe(self.update_wms_visibility)
-        # layer_header.value = f'<hr><h2>{self.ztwms.name}</h2><hr>'
-        layer_footer = HTML()
-        layer_footer.value = '<br>'
+        # layer_footer = HTML()
+        # layer_footer.value = '<br>'
         # WMS Layer selection
         layer_selector = Dropdown(
             options=self.wms_layers,
             value=self.wms_layers[0],
             description='<font style="text-align:left;"><b>Layer:</b></font>',
-            layout=Layout(max_width="280px", max_height="35px"),
+            # layout=Layout(max_width="380px", max_height="35px"),
         )
         layer_selector.observe(self.update_wms_layer, "value")
         # WMS Opacity slider
@@ -124,7 +104,7 @@ class ztwms_control(object):
             options=op_range,
             orientation="horizontal",
             value=1,
-            layout=Layout(max_width="280px", max_height="35px"),
+            # layout=Layout(max_width="380px", max_height="35px"),
         )
         opacity_slider.observe(self.update_wms_opacity, "value")
         # WMS TIME
@@ -132,7 +112,7 @@ class ztwms_control(object):
             time_slider = SelectionSlider(
                 description="<b>Time:</b>",
                 options=self.wms.contents[layer_selector.value].timepositions,
-                layout=Layout(max_width="280px", max_height="35px"),
+                # layout=Layout(max_width="380px", max_height="35px"),
             )
             time_slider.layout.visibility = "visible"
             # print(self.wms.contents[layer_selector.value].timepositions)
@@ -140,7 +120,7 @@ class ztwms_control(object):
             time_slider = SelectionSlider(
                 description="<b>Time:</b>",
                 options=[""],
-                layout=Layout(max_width="280px", max_height="35px"),
+                # layout=Layout(max_width="380px", max_height="35px"),
             )
             time_slider.layout.visibility = "hidden"
         time_slider.observe(self.update_wms_time, "value")
@@ -149,7 +129,7 @@ class ztwms_control(object):
             elevation_slider = SelectionSlider(
                 description="<b>Elevation:</b>",
                 options=self.wms.contents[layer_selector.value].elevations,
-                layout=Layout(max_width="280px", max_height="35px"),
+                # layout=Layout(max_width="380px", max_height="35px"),
             )
             elevation_slider.layout.visibility = "visible"
             # print(self.wms.contents[layer_selector.value].elevations)
@@ -157,7 +137,7 @@ class ztwms_control(object):
             elevation_slider = SelectionSlider(
                 description="<b>Elevation:</b>",
                 options=[""],
-                layout=Layout(max_width="280px", max_height="35px"),
+                # layout=Layout(max_width="380px", max_height="35px"),
             )
             elevation_slider.layout.visibility = "hidden"
         elevation_slider.observe(self.update_wms_elevation, "value")
@@ -166,7 +146,7 @@ class ztwms_control(object):
             styles_selector = Dropdown(
                 description="<b>Style:</b>",
                 options=list(self.wms.contents[layer_selector.value].styles.keys()),
-                layout=Layout(max_width="280px", max_height="35px"),
+                # layout=Layout(max_width="380px", max_height="35px"),
             )
             styles_selector.layout.visibility = "visible"
             wms_legend = HTML(
@@ -185,8 +165,6 @@ class ztwms_control(object):
 
         return VBox(
             [
-                #layer_header,
-                # head,
                 header,
                 layer_selector,
                 opacity_slider,
@@ -194,15 +172,14 @@ class ztwms_control(object):
                 time_slider,
                 elevation_slider,
                 wms_legend,
-                layer_footer,
-            ], layout=Layout(border='solid 1px')
+            ],  # layout=Layout(border='solid 1px', min_height="200px", display='block')
         )
 
     def map_interact_up(self, change):
         maplayers = list(self.m.layers)[::-1]
         for i, v in enumerate(maplayers):
             if hasattr(v, 'ipygis_key'):
-                if v.ipygis_key==self.ipygis_key:
+                if v.ipygis_key == self.ipygis_key:
                     key_index = 1
                     key_layer = v
                     print('ok', v.name, i)
@@ -210,17 +187,17 @@ class ztwms_control(object):
         self.m.add_layer(key_layer)
         self.update_wms_layer(change)
         self.wms_control = self.get_wms_controls()
-        #maplayers.insert(key_index-1, maplayers.pop(key_index))
-        #self.m.layers = maplayers
+        # maplayers.insert(key_index-1, maplayers.pop(key_index))
+        # self.m.layers = maplayers
 
     def map_interact_down(self, change):
         maplayers = list(self.m.layers)[::-1]
         for i, v in enumerate(maplayers):
             if hasattr(v, 'ipygis_key'):
-                if v.ipygis_key==self.ipygis_key:
+                if v.ipygis_key == self.ipygis_key:
                     key_index = 1
                     print('ok', v.name, i)
-        maplayers.insert(key_index+1, maplayers.pop(key_index))
+        maplayers.insert(key_index + 1, maplayers.pop(key_index))
         self.m.layers = maplayers
 
     def update_wms_visibility(self, change):
@@ -292,5 +269,3 @@ class ztwms_control(object):
             self.wms_control.children[3].layout.visibility = "hidden"
         self.ztwms.layers = self.wms_control.children[1].value
         self.wms_control.children[1].layout.visibility = "visible"
-
-
